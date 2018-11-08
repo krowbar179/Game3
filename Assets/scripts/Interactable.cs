9275aa95a5d1a26d59c667924f3204b8862d3a80
Ben Krowitz <krowbar179@gmail.com>
2018-11-08 09:52:14 -0500

    Started work on Interactable scripts and event handling
    Help

---
+++ Assets/scripts/Interactable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Interactable : MonoBehaviour, IPointerClickHandler
+{
+    private Rigidbody body;
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData data)
+    {
+        body.AddForce(new Vector3(0, 10, 0), ForceMode.VelocityChange);
+    }
+}
